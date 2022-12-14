cb73036c6dacf4ca9193756ef81e1bae6df278fe
moranobam <moranobam@gmail.com>
2022-12-14 19:38:06 +0000

    new game

--- Mini_project_2048.py
+++ Mini_project_2048.py
@@ -11,45 +11,61 @@ def merge():
     print("\n")
     user_list = list(map(int, input_string.split()))
 
-    for num, name in enumerate(user_list):
-    #list is here
-        user_list = list(map(int, input_string.split()))
-    #loop starts (takes the value from the list)
     should_restart = True
     while should_restart:
         should_restart = False
+        value_in_place = 0
+        index_in_place = 0
+        index_zero = 0
+        merge = 0
         for index, value in enumerate(user_list):
-            value_in_place = 0
-            index_in_place = 0
-            index_zero = 0 
             if index == 0:
                 value_in_place = value
+                index_in_place = index
                 continue
-            if index_in_place > 0 and value > 0 and value == value_in_place:
+            elif value == 0:
+                value_in_place = value
+                index_in_place = index
+                index_zero = index
+                continue
+            elif index  > 0 and value > 0 and value == value_in_place:
                 user_list[index_in_place] = value_in_place + value
                 value_in_place = 0
-                index_in_place = value
-                continue
-            if index_zero > 0 :
-                user_list[index_zero] = value
-                index_zero = user_list.index(value)
                 user_list[index] = 0
-                index_zero = 0
+                index_in_place = index
+                index_zero = index
                 continue
-            if index == 0:
-                # set the current value to the value_in_place variable.
-                value_in_place = value
-                continue
-            if index == len(user_list) - 1:
-                    print(user_list)
-                    loop_continue =  input("Press Enter to continue...")
-                    if loop_continue == "":
-                        should_restart = True
-                    else:
-                        print(user_list)  
-                        break  
+            elif index  > 0 and value >= 0 and value != value_in_place:
+                if index_zero > 0 :
+                    user_list[index_zero] = value
+                    user_list[index] = 0
+                    index_zero = index
+                    if user_list[index-2] == value and merge == 0:
+                        user_list[index-2] = value + user_list[index-2]
+                        value_in_place = 0
+                        user_list[index-1] = 0
+                        index_in_place = index
+                        index_zero = index-1
+                        merge = 1
+                        continue
+                continue 
+        print(user_list)
+
+        # iterator = iter(user_list)
+        # first = next(iterator)
+        # if not all(first == x for x in iterator):
+        #     should_restart = 
+            
+
+         
+    #     loop_continue =  input("Press Enter to continue...")
+    #     if loop_continue == "":
+    #         should_restart = True
+    #         else:
+    #             print(user_list)  
+    #             break  
             # if index_in_place > 0 and value == 0 :
                 # it take the index of this value index_zero
                 # value_in_place =0 
                 #continue
-
+merge()
