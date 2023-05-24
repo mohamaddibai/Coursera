f73e18f69cfaa88ae6d84aa329f697dcf050d400
Moranobam <54619696+mohamaddibai@users.noreply.github.com>
2023-05-24 18:15:05 +0100

    new

---
+++ Grid.py
@@ -0,0 +1,10 @@
+
+GRID_HEIGHT = 4
+GRID_WIDTH = 6
+
+# Create a rectangular grid using nested list comprehension 
+# Inner comprehension creates a single row
+EXAMPLE_GRID = [[row + col for col in range(GRID_WIDTH)]
+                           for row in range(GRID_HEIGHT)]
+
+print(EXAMPLE_GRID)
